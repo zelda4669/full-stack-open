a35be12055abeaa79a5fc753715f97740abf4cae
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-03-18 12:01:23 -0700

    Exercise 1.7

--- part1/unicafe-app/src/App.js
+++ part1/unicafe-app/src/App.js
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import { mean, multiply } from 'mathjs'
 
 const Header = (props) => <div><h1>{props.text}</h1></div>
 
-const Display = (props) => <div>{props.stat}: {props.value}</div>
+const Display = (props) => <div>{props.stat}: {props.value} {props.unit}</div>
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const avg = mean(good, neutral, bad)
+  const percent = multiply(good/total, 100) 
 
   return (
     <div>
@@ -26,6 +30,10 @@ const App = () => {
       <Display stat='Good' value={good} />
       <Display stat='Neutral' value={neutral} />
       <Display stat='Bad' value={bad} />
+      <Display stat='Total' value={total} />
+      <Display stat='Average' value={avg} />
+      <Display stat='Percent Positive' value={percent} unit='%' />
+
     </div>
   )
 }
