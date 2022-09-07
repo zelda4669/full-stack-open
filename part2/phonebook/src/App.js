d6a83472ff1403525fb2ca0ce37d8b81deaf3ef8
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-09-06 23:15:38 -0700

    Exercise 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -33,9 +33,14 @@ const App = () => {
     if (names.includes(newName) === true) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(personObject))
+          setNewName('')
+          setNewNumber('')
+        })
+     
     }
   }
 
