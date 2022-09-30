c451a0552a2e88a8ae7c6807543acaf1ce581daf
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-09-30 09:45:04 -0700

    Exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,11 +6,33 @@ function totalLikes(blogs) {
     const likes = []
     blogs.forEach(b => {
         likes.push(b.likes)
-    });
+    })
     return likes.reduce((p, c) => p + c, 0)
+}
+
+function favoriteBlog(blogs) {
+    if(blogs.length === 0) {
+        return []
+    } else {
+        const likes = []
+        blogs.forEach(b => {
+            likes.push(b.likes)
+        })
+        let mostLikes = Math.max(...likes)
+        let bestBlog = blogs.filter(blog => blog.likes === mostLikes)
+
+        return {
+            title: bestBlog[0].title,
+           author: bestBlog[0].author,
+           likes: bestBlog[0].likes
+       }
+    }
+   
 }
 
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
+
 }
